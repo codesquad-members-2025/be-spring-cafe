277f2edc642f68f7786e70fb604b4b305342c599
Hoyoung Jung <honux77@gmail.com>
2025-03-14 16:40:05 +0900

    스프링 프로젝트 초기화

---
+++ src/main/java/codesquad/codestagram/CodestagramApplication.java
@@ -0,0 +1,13 @@
+package codesquad.codestagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodestagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodestagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codesquad/codestagram/CodestagramApplicationTests.java
@@ -0,0 +1,13 @@
+package codesquad.codestagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodestagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
