645dace1a9d757d1b33eb656e1dd3315b62cf6e6
jangsubin <eltm521@naver.com>
2025-03-17 12:02:06 +0900

    feat : 회원프로필 정보보기 기능 구현 - @PathVariable로 userId가 같은 회원을 찾는다

--- src/main/java/codesquad/codestagram/controller/UserController.java
+++ src/main/java/codesquad/codestagram/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -26,5 +27,14 @@ public class UserController {
     public String showUsers(Model model) {
         model.addAttribute("users", users);
         return "user/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String showUserProfile(@PathVariable String userId, Model model) {
+        for (User user : users) {
+            if (user.equalUserId(userId))
+                model.addAttribute("user", user);
+        }
+        return "user/profile";
     }
 }
