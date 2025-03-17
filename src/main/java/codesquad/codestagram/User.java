ecc9baf09a3700972841fa72199897ed02fc677b
jangsubin <eltm521@naver.com>
2025-03-17 11:18:22 +0900

    feat : User 생성 - 회원 가입시 생성할 User객체 생성

---
+++ src/main/java/codesquad/codestagram/User.java
@@ -0,0 +1,15 @@
+package codesquad.codestagram;
+
+public class User {
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+}
