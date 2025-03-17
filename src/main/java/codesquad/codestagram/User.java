2d7fbbc90b32d26b315694c31e8a4830012201bf
jangsubin <eltm521@naver.com>
2025-03-17 12:02:43 +0900

    feat : equalUserId구현 - 입력값이 userId와 같으면 true

--- src/main/java/codesquad/codestagram/User.java
+++ src/main/java/codesquad/codestagram/User.java
@@ -28,4 +28,8 @@ public class User {
     public String getEmail() {
         return email;
     }
+
+    public boolean equalUserId(String userId) {
+        return this.userId.equals(userId);
+    }
 }
