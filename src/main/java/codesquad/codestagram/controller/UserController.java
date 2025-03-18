b896c0e00ddf4dc964f286996ec3b11a2ab47549
jang-jinuk <dlsndifn98@gmail.com>
2025-03-18 12:14:50 +0900

    feat: 회원가입 엔드포인트 추가
    - POST /users로 오는 요청을 처리하도록 구현했습니다.
    - 받아온 값을 User 객체 형태로 담아 List에 저장하도록 구현했습니다.

--- src/main/java/codesquad/codestagram/controller/UserController.java
+++ src/main/java/codesquad/codestagram/controller/UserController.java
@@ -1,10 +1,23 @@
 package codesquad.codestagram.controller;
 
+import codesquad.codestagram.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    List<User> userList = new ArrayList<>();
+
+    @PostMapping
+    public String signup(@ModelAttribute User user) {
+        userList.add(user);
+        return "redirect:/users";
+    }
 }
