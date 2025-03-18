80e5b7614ec5bf6d7c92701f9daabf2deb2386e7
2jiyong <jdragon2000@naver.com>
2025-03-18 15:33:41 +0900

    Feat: MvcConfig로 기본 매핑 추가

---
+++ src/main/java/codesquad/codestagram/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package codesquad.codestagram.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {  // ✅ 인터페이스로 변경!
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/user/login").setViewName("user/login");
+        registry.addViewController("/user/form").setViewName("user/form");
+        registry.addViewController("/qna/form").setViewName("qna/form");
+        registry.addViewController("/qna/show").setViewName("qna/show");
+    }
+}
