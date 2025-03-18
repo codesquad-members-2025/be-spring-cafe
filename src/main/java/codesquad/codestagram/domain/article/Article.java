e6aa86229c1e4cb2d07fe44efef4f42941c345e9
gladhee <gladhee@icloud.com>
2025-03-18 12:32:08 +0900

    feat(Article): Article 모델 작성
    - id, title, content

---
+++ src/main/java/codesquad/codestagram/domain/article/Article.java
@@ -0,0 +1,39 @@
+package codesquad.codestagram.domain.article;
+
+public class Article {
+
+    private String id;
+    private String title;
+    private String content;
+
+    public Article(String id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
