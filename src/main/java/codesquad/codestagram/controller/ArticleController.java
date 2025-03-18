60c02624ea6f5dc5817b2d66cf12e9cb2012d893
johnnie <come753zz@gmail.com>
2025-03-18 21:34:03 +0900

    feat - 글쓰기 기능 추가 1. 새로운 글 쓰기 기능 2. 전체 글 목록 조회하기 기능 3. 전체 글 중 특정 글에 대해 게시글 상세보기 기능

---
+++ src/main/java/codesquad/codestagram/controller/ArticleController.java
@@ -0,0 +1,49 @@
+// ArticleController.java
+package codesquad.codestagram.controller;
+
+import codesquad.codestagram.entity.Article;
+import codesquad.codestagram.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/qna")
+public class ArticleController {
+
+    @Autowired  // @Autowired 애너테이션이 정확히 붙어 있는지 확인
+    private ArticleService service;
+
+    @GetMapping()
+    public String viewQuestions(Model model) {
+        List<Article> articles = service.getArticles();
+        model.addAttribute("articles", articles);
+        return "qna/show";
+    }
+
+    @GetMapping("/articles/{index}")
+    public String viewArticle(@PathVariable("index") int index, Model model) {
+        List<Article> articles = service.getArticles();
+        if (index > 0 && index <= articles.size()) {
+            Article article = articles.get(index - 1);
+            model.addAttribute("article", article);
+            return "qna/show";
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/form")
+    public String showForm() {
+        return "qna/form";
+    }
+
+    @PostMapping("/write")
+    public String submitArticle(@RequestParam("title") String title,@RequestParam("content") String content) {
+        Article article = new Article(service.getArticles().size() + 1, title, content);
+        service.addArticle(article);
+        return "redirect:/qna";
+    }
+}
