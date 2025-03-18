1d0417277c07ccc38003f95d05ac4ab572d5cb98
guzzang <991012kim@gmail.com>
2025-03-18 17:28:29 +0900

    Feat: 회원 프로필 조회 기능 구현
    회원 프로필 조회 API 개발

--- src/main/java/codesquad/codestagram/controller/UserController.java
+++ src/main/java/codesquad/codestagram/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -24,6 +25,15 @@ public class UserController {
         model.addAttribute("users", userList);
         return "user/list";
     }
+
+    @GetMapping("/users/{userId}")
+    public String getUser(@PathVariable String userId, Model model){
+        for (User user : userList) {
+            if(user.getUserId().equals(userId)) model.addAttribute("user", user);
+        }
+        return "user/profile";
+    }
+
 
 
 
