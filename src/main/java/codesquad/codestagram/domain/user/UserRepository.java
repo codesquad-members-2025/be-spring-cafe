0e1b280e160f66cee48b0c96ae10f80a42cbdd04
gladhee <gladhee@icloud.com>
2025-03-18 14:42:32 +0900

    fix(UserRepository): users 접근 제한자 및 final 추가

--- src/main/java/codesquad/codestagram/domain/user/UserRepository.java
+++ src/main/java/codesquad/codestagram/domain/user/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Repository
 public class UserRepository {
 
-    List<User> users;
+    private final List<User> users;
 
     public UserRepository(List<User> users) {
         this.users = users;
