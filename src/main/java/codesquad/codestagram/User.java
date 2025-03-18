bd380dede3442092da201d0270d4bccbff3f99db
jangsubin <eltm521@naver.com>
2025-03-18 15:01:21 +0900

    feat : 회원 식별자 추가 - 회원을 고유하게 판단하기 위홰 id 및 counter 추가

--- src/main/java/codesquad/codestagram/User.java
+++ src/main/java/codesquad/codestagram/User.java
@@ -1,16 +1,23 @@
 package codesquad.codestagram;
 
 public class User {
+    private static int idCounter = 1;
+    private int id;
     private String userId;
     private String password;
     private String name;
     private String email;
 
     public User(String userId, String password, String name, String email) {
+        id = idCounter++;
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getUserId() {
