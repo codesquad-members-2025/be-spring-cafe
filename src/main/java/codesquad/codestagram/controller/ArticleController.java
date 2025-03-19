8e8384e5c08afad3a6a353f7b441d97a15b0c6f1
jangsubin <eltm521@naver.com>
2025-03-19 11:27:00 +0900

    feat : Article db 연결

--- src/main/java/codesquad/codestagram/controller/ArticleController.java
+++ src/main/java/codesquad/codestagram/controller/ArticleController.java
@@ -1,7 +1,10 @@
 package codesquad.codestagram.controller;
 
 import codesquad.codestagram.domain.Article;
+import codesquad.codestagram.dto.ArticleDto;
+import codesquad.codestagram.repository.ArticleRepository;
 import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,27 +14,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
-    private final ArrayList<Article> articles;
+    private final ArticleRepository articleRepository;
 
-    public ArticleController(ArrayList<Article> articles) {
-        this.articles = articles;
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
     }
 
     @PostMapping("/articles")
-    public String writeArticle(@ModelAttribute Article article) {
-        articles.add(article);
+    public String writeArticle(@ModelAttribute ArticleDto.ArticleRequestDto requestDto) {
+        Article article = requestDto.toArticle();
+        articleRepository.save(article);
         return "redirect:/";
     }
 
     @GetMapping("/")
     public String showArticles(Model model) {
+        List<Article> articles = articleRepository.findAll();
         model.addAttribute("articles", articles);
         return "/home";
     }
 
     @GetMapping("articles/{articleId}")
-    public String showArticleDetail(@PathVariable int articleId, Model model) {
-        Article article = articles.get(articleId - 1);
+    public String showArticleDetail(@PathVariable Long articleId, Model model) {
+        Article article = articleRepository.findById(articleId).get();
         model.addAttribute(article);
         return "articles/show";
     }
