fb548ba7f0589776a4b3edd95a610215eb4a540c
jangsubin <eltm521@naver.com>
2025-03-19 10:45:57 +0900

    feat : UserRepository 생성

---
+++ src/main/java/codesquad/codestagram/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package codesquad.codestagram.repository;
+
+import codesquad.codestagram.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
