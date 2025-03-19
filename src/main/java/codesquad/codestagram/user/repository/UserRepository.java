105025b974a72c83692704ec3151a1a337fa9eaf
jaehoonchoi <eeoos8840@gmail.com>
2025-03-19 11:07:45 +0900

    Fix(UserRepository): save 메서드 리턴 타입 변경
    - save 메서드의 리턴 타입을 User에서 void로 변경

--- src/main/java/codesquad/codestagram/user/repository/UserRepository.java
+++ src/main/java/codesquad/codestagram/user/repository/UserRepository.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
-    User save(User user);
-    Optional<User> findBySeq();
+    Long save(User user);
+    Optional<User> findBySeq(Long seq);
     List<User> findAll();
 
 }
