7c68077dcc2973b9f738d96eb626d5b0c47ecdd4
moongyuxx <gyuwon822@gmail.com>
2025-03-19 16:31:51 +0900

    feat: 회원 객체 생성

---
+++ src/main/java/codesquad/codestagram/domain/User.java
@@ -0,0 +1,40 @@
+package codesquad.codestagram.domain;
+
+public class User {
+    private Long id;
+    private String name;
+    private String password;
+    private String email; //프로필 페이지에서 필요
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
