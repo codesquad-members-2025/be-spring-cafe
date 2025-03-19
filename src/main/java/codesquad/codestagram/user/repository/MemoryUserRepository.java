390253af01e1c0879527994b3b2a2a14b04fa5bb
jaehoonchoi <eeoos8840@gmail.com>
2025-03-19 11:11:08 +0900

    Feat(MemoryUserRepository): UserRepository의 구현체 MemoryUserRepository 클래스 생성
    - Map에 회원을 저장

--- src/main/java/codesquad/codestagram/user/repository/MemoryUserRepository.java
+++ src/main/java/codesquad/codestagram/user/repository/MemoryUserRepository.java
@@ -1,2 +1,33 @@
-package codesquad.codestagram.user.repository;public class MemoryUserRepository {
+package codesquad.codestagram.user.repository;
+
+import codesquad.codestagram.user.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class MemoryUserRepository implements UserRepository {
+
+    private static final Map<Long, User> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public void save(User user) {
+        user.setSeq(++sequence);
+        store.put(user.getSeq(), user);
+    }
+
+    @Override
+    public Optional<User> findBySeq(Long seq) {
+        return Optional.ofNullable(store.get(seq));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
