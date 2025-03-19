4a43e85135ec53ba6f0afe91ed2eab19cf0fa8d7
jaehoonchoi <eeoos8840@gmail.com>
2025-03-19 10:39:57 +0900

    Feat(User): User 클래스 생성
    - 식별자, 아이디, 비밀번호, 이름, 이메일을 필드로 갖는 User 클래스 생성

---
+++ src/main/java/codesquad/codestagram/user/domain/User.java
@@ -0,0 +1,17 @@
+package codesquad.codestagram.user.domain;
+
+public class User {
+
+    private Long seq;
+    private String id;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(String id, String password, String name, String email) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+}
