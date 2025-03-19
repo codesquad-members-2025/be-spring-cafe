10dd71f9f68794d950363e9a217b791e4e4ba96d
2jiyong <jdragon2000@naver.com>
2025-03-19 16:26:06 +0900

    Feat: article에 시간 추가 index.html에서도 article에서 시간을 보여주도록 함.

--- src/main/java/codesquad/codestagram/domain/Article.java
+++ src/main/java/codesquad/codestagram/domain/Article.java
@@ -1,16 +1,24 @@
 package codesquad.codestagram.domain;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Article {
     private String title;
     private String content;
     private User user;
     private int id;
+    private String createdAt;
 
     public Article(User user, String title, String content, int id) {
         this.user = user;
         this.title = title;
         this.content = content;
         this.id = id;
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        this.createdAt = now.format(dateFormatter)+" "+now.format(timeFormatter);
     }
 
     public User getUser() {
@@ -43,5 +51,9 @@ public class Article {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
     }
 }
