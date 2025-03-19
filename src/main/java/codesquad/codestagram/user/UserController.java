17595f8d10f98f6c863b5c75b76d6371831bcbde
jaehoonchoi <eeoos8840@gmail.com>
2025-03-19 10:13:46 +0900

    Feat(UserController): userService 의존성 주입, 회원정보를 받는 PostMapping 생성

--- src/main/java/codesquad/codestagram/user/UserController.java
+++ src/main/java/codesquad/codestagram/user/UserController.java
@@ -1,13 +1,37 @@
 package codesquad.codestagram.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/signUp")
     public String signUpForm() {
         return "user/form";
+    }
+
+    @PostMapping("/signUp")
+    public String signUp(SignUpRequest request) {
+        User user = new User(
+                request.getId(),
+                request.getPassword(),
+                request.getName(),
+                request.getEmail()
+        );
+
+        userService.join(user);
+        
+        return "redirect:/users";
     }
 }
