59636588c66a6f9e913490fe16560753eeeb5889
jaehoonchoi <eeoos8840@gmail.com>
2025-03-19 10:48:34 +0900

    Feat(UserService): UserService 클래스 생성
    - 회원을 저장하는 join 메서드 추가

--- src/main/java/codesquad/codestagram/user/service/UserService.java
+++ src/main/java/codesquad/codestagram/user/service/UserService.java
@@ -1,2 +1,23 @@
-package codesquad.codestagram.user.service;public class UserService {
+package codesquad.codestagram.user.service;
+
+import codesquad.codestagram.user.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Long join(User user) {
+        userRepository.save(user);
+        return user.getSeq();
+    }
+
+    
 }
