317b17da97ed038f0c13396d50091ae31c10ea7e
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-19 11:15:51 +0900

    feat: UserService 클래스 구현
    비즈니스 로직을 수행하는 클래스 구현

---
+++ src/main/java/codesquad/codestagram/service/UserService.java
@@ -0,0 +1,38 @@
+package codesquad.codestagram.service;
+
+import codesquad.codestagram.domain.User;
+import codesquad.codestagram.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // 회원가입
+    public String join(User user) {
+        validateDuplication(user);
+        userRepository.save(user);
+        return user.getId();
+    }
+
+    private void validateDuplication(User user) {
+        userRepository.findById(user.getId())
+                .ifPresent(u -> {
+                    throw new IllegalStateException("사용할 수 없는 아이디입니다.");
+                });
+    }
+
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findOne(String userId) {
+        return userRepository.findById(userId);
+    }
+}
