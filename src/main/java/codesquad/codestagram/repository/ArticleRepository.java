fe20735523aa07b8d7538f7261e31c7c26bf6621
2jiyong <jdragon2000@naver.com>
2025-03-19 11:20:01 +0900

    Feat: ArticleRepository 인터페이스 생성

---
+++ src/main/java/codesquad/codestagram/repository/ArticleRepository.java
@@ -0,0 +1,14 @@
+package codesquad.codestagram.repository;
+
+import codesquad.codestagram.domain.Article;
+import codesquad.codestagram.domain.User;
+
+import java.util.List;
+
+public interface ArticleRepository {
+    Article save(Article article);
+    List<Article> findByUser(User user);
+    List<Article> findByTitle(String title);
+    List<Article> findAll();
+    void clearStore();
+}
