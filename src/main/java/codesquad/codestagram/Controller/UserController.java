ef98ab86aedd4bad676c178f621740cf21f4dfbe
moongyuxx <gyuwon822@gmail.com>
2025-03-19 17:53:32 +0900

    refactor: 회원 컨트롤러에 의존관계 추가

---
+++ src/main/java/codesquad/codestagram/Controller/UserController.java
@@ -0,0 +1,15 @@
+package codesquad.codestagram.Controller;
+
+import codesquad.codestagram.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
