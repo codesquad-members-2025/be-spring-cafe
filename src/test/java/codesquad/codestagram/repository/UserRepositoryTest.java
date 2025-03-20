0d1f2fd93a0fa1aa790e13038a93a4dfe9ea9b55
jang-jinuk <dlsndifn98@gmail.com>
2025-03-20 20:54:36 +0900

    test: 전체 회원 조회 기능 검증
    findAll 메소드가 회원정보를 모두 조회해오는지 검증하는 테스트를 작성했습니다.

--- src/test/java/codesquad/codestagram/repository/UserRepositoryTest.java
+++ src/test/java/codesquad/codestagram/repository/UserRepositoryTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 public class UserRepositoryTest {
 
@@ -41,5 +43,28 @@ public class UserRepositoryTest {
         softly.assertThat(user.getUserId()).isNotNull();
         softly.assertThat(user.getUserId()).isEqualTo(expectedUser.getUserId());
         softly.assertAll();
+    }
+
+    @Test
+    @DisplayName("저장된 회원은 두명이다.")
+    void findById() {
+        //Given
+        User user1 = new User();
+        user1.setUserId("testId1");
+        user1.setName("tester2");
+        user1.setPassword("1234");
+        userRepository.save(user1);
+
+        User user2 = new User();
+        user2.setUserId("testId2");
+        user2.setName("tester2");
+        user2.setPassword("1234");
+        userRepository.save(user2);
+
+        //When
+        List<User> users = userRepository.findAll();
+
+        //Then
+        softly.assertThat(users.size()).isEqualTo(2);
     }
 }
