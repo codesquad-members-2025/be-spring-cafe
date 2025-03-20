7c388e87880c411a31578894217cc555133b2815
jang-jinuk <dlsndifn98@gmail.com>
2025-03-20 15:03:10 +0900

    feat: 메인페이지 엔드 포인트 추가
    - GET / 요청이 왔을 때 articles 리스트를 Model 객체에 담아 메인 페이지를 반환합니다.

---
+++ src/main/java/codesquad/codestagram/controller/MainController.java
@@ -0,0 +1,26 @@
+package codesquad.codestagram.controller;
+
+import codesquad.codestagram.domain.Article;
+import codesquad.codestagram.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @GetMapping
+    public String getMain(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "main";
+    }
+}
