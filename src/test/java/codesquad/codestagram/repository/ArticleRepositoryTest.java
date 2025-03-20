c804585046ab9e1577db88210427d9b46479a53f
jang-jinuk <dlsndifn98@gmail.com>
2025-03-20 12:46:51 +0900

    test: 글 리스트 조회 기능 검증
    - findAll 메소드가 모든 글 리스트를 잘 조회하는지 검증하는 테스트 코드를 작성했습니다.

--- src/test/java/codesquad/codestagram/repository/ArticleRepositoryTest.java
+++ src/test/java/codesquad/codestagram/repository/ArticleRepositoryTest.java
@@ -4,11 +4,13 @@ import codesquad.codestagram.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ArticleRepositoryTest {
 
-    private ArticleRepository articleRepository;
+    private ArticleRepository articleRepository = new ArticleRepository();
 
     @Test
     @DisplayName("1번 글이 작성됩니다.")
@@ -24,5 +26,26 @@ public class ArticleRepositoryTest {
 
         //Then
         assertThat(article.getId()).isEqualTo(expected.getId());
+    }
+
+    @Test
+    @DisplayName("모든 글의 갯수는 2개입니다.")
+    void findAll() {
+        //Given
+        Article article1 = new Article();
+        article1.setTitle("안녕하세요");
+        article1.setContent("반갑습니다.");
+        articleRepository.save(article1);
+
+        Article article2 = new Article();
+        article2.setTitle("반갑습니다.");
+        article2.setContent("안녕하세요.");
+        articleRepository.save(article2);
+
+        //When
+        List<Article> articles = articleRepository.findAll();
+
+        //Then
+        assertThat(articles.size()).isEqualTo(2);
     }
 }
