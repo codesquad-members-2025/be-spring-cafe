eab60c54b5e6e2c0a3ef2a77306c3a71323117e5
dorkem <20233144@gs.cwnu.ac.kr>
2025-03-21 01:22:44 +0900

    :sparkles: 코멘트 객체 생성

---
+++ src/main/java/codesquad/codestagram/Entity/Comment.java
@@ -0,0 +1,48 @@
+package codesquad.codestagram.Entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+    private LocalDateTime createdAt;
+
+    public Comment() {}
+    public Comment(String content, User user, Board board) {
+        this.content = content;
+        this.user = user;
+        this.board = board;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+    public Board getBoard() { return board; }
+    public void setBoard(Board board) { this.board = board; }
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
