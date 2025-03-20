3b4ecd0becb1532ca0284844f386e607ea008ba3
jang-jinuk <dlsndifn98@gmail.com>
2025-03-20 12:48:55 +0900

    feat: Article 클래스 id 필드 추가
    - 글마다 고유성을 보장하기 위해 id 필드를 추가했습니다.

--- src/main/java/codesquad/codestagram/domain/Article.java
+++ src/main/java/codesquad/codestagram/domain/Article.java
@@ -1,19 +1,28 @@
 package codesquad.codestagram.domain;
 
 public class Article {
+    private Long id;
     private String title;
     private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
     }
 
-    public String getContent() {
-        return content;
-    }
-
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getContent() {
+        return content;
     }
 
     public void setContent(String content) {
