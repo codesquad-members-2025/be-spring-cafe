57e11ef2f3496f015512bcbbf425b644de7e0308
심재연 <jaeyeonsim@simjaeyeon-ui-MacBookPro.local>
2025-03-20 12:27:15 +0900

    feat: 회원가입, 조회 기능 추가

--- src/main/java/codesquad/codestagram/controller/UserController.java
+++ src/main/java/codesquad/codestagram/controller/UserController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -28,16 +32,36 @@ public class UserController {
         return "user/form";
     }
 
-    @PostMapping("/form")
-    public String createForm(UserForm form) {
+    @PostMapping("/user/create")
+    public String create(
+            @RequestParam("userId") String userId,
+            @RequestParam("password") String password,
+            @RequestParam("name") String name,
+            @RequestParam("email") String email) {
         User user = new User();
-        user.setId(form.getId());
-        user.setNickname(form.getNickname());
-        user.setPassWord(form.getPassword());
+        user.setUserId(userId);
+        user.setName(name);
+        user.setPassword(password);
+        user.setEmail(email);
 
         userService.join(user);
 
         return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model) {
+        List<User> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "/user/list";
+    }
+
+    @GetMapping("/user/{userId}")
+    public String profile(@PathVariable("userId") String userId, Model model) {
+        User user = userService.findOne(userId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
+        model.addAttribute("user", user);
+        return "/user/profile";
     }
 
 }
