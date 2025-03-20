d7ea4d9ab22f4c841f3b661ec300fe2f486a4ee7
dorkem <20233144@gs.cwnu.ac.kr>
2025-03-21 01:29:29 +0900

    :recycle: 유저 레포 변경
    도저히 user로 못찾아오길래 변경함

--- src/main/java/codesquad/codestagram/repository/UserRepository.java
+++ src/main/java/codesquad/codestagram/repository/UserRepository.java
@@ -3,9 +3,9 @@ package codesquad.codestagram.repository;
 import codesquad.codestagram.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 
 @Repository("userJpaRepository")
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findById(String id);
+    Optional<User> findById(String id);
 }
