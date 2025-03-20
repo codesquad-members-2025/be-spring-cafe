485e2cd7682331c97c548d1a626ed66df2311687
gladhee <gladhee@icloud.com>
2025-03-20 15:40:29 +0900

    feat(data.sql): 테스트데이터 init 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (user_id, password, name, email)
+VALUES ('javajigi', 'test', '자바지기', 'javajigi@slipp.net');
+
+INSERT INTO users (user_id, password, name, email)
+VALUES ('sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
