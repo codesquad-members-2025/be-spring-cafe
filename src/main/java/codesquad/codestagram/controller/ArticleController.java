4882dbf64476fb01d65d044eb53592635810ca47
jang-jinuk <dlsndifn98@gmail.com>
2025-03-20 15:05:35 +0900

    refactor: ArticleRepository 도입
    - 다른 컨트롤러에서도 동일한 글 리스트를 사용할 수 있도록 ArticleRepository를 통해 글을 관리하도록 구현했습니다.

--- src/main/java/codesquad/codestagram/controller/ArticleController.java
+++ src/main/java/codesquad/codestagram/controller/ArticleController.java
@@ -1,24 +1,23 @@
 package codesquad.codestagram.controller;
 
 import codesquad.codestagram.domain.Article;
+import codesquad.codestagram.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
 
-    private List<Article> articles = Collections.synchronizedList(new ArrayList<>());
+    @Autowired
+    ArticleRepository articleRepository;
 
     @PostMapping
     public String writeArticle(@ModelAttribute Article article) {
-        articles.add(article);
+        articleRepository.save(article);
         return "redirect:/";
     }
 }
