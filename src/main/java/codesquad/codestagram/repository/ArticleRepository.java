07271b6ac7af63620dbcb0bace5d16fc15823be7
jang-jinuk <dlsndifn98@gmail.com>
2025-03-20 12:51:04 +0900

    feat: 글 저장 메소드 구현
    - 글정보가 담긴 Artile 객체를 articles 리스트에 저장하도록 구현했습니다.
    - 리스트에 저장되기 전에 교유한 id값을 부여 받습니다.

---
+++ src/main/java/codesquad/codestagram/repository/ArticleRepository.java
@@ -0,0 +1,21 @@
+package codesquad.codestagram.repository;
+
+import codesquad.codestagram.domain.Article;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ArticleRepository {
+
+    private static List<Article> articles = new ArrayList<>();
+    private static long sequence = 0L;
+
+    public Article save(Article article) {
+        article.setId(++sequence);
+        articles.add(article);
+        return article;
+    }
+}
