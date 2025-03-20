178bc200b15ed6af269a1fae343eac814115680e
moongyuxx <gyuwon822@gmail.com>
2025-03-20 19:57:05 +0900

    fear: board 클래스 생성 * 게시글 데이터를 저장할 board 엔티티 클래스

---
+++ src/main/java/codesquad/codestagram/domain/Board.java
@@ -0,0 +1,48 @@
+package codesquad.codestagram.domain;
+
+public class Board {
+    private Long boardId;
+    private String title;
+    private String content;
+    private String writer;
+    //아직 로그인 기능 없음 -> writer만 저장
+
+    public Board(Long id, String title, String content, String writer) {
+        this.boardId = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
+
+    public Long getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(Long boardId) {
+        this.boardId = boardId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+}
