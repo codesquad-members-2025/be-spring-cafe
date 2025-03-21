9374a6f825678a96d8528f7fa05cc0299779dad6
jang-jinuk <dlsndifn98@gmail.com>
2025-03-21 10:49:36 +0900

    refactor: UserController 구조변경
    - UserRepository 의존성 주입을 해 UserRepository를 통해 회원 정보를 다루도록 구조를 변경했습니다.

--- src/main/java/codesquad/codestagram/controller/UserController.java
+++ src/main/java/codesquad/codestagram/controller/UserController.java
@@ -1,49 +1,47 @@
 package codesquad.codestagram.controller;
 
 import codesquad.codestagram.domain.User;
+import codesquad.codestagram.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
-    private final List<User> users = Collections.synchronizedList(new ArrayList<>());
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping
     public String signup(@ModelAttribute User user) {
-        users.add(user);
+        userRepository.save(user);
         return "redirect:/users";
     }
 
     @GetMapping
     public String viewUserList(Model model) {
+        List<User> users = userRepository.findAll();
         model.addAttribute("users", users);
         return "user/list";
     }
 
     @GetMapping("/{id}")
-    public String viewUserProfile(@PathVariable("id") String id, Model model) {
-        for (User user : users) {
-            if (user.getUserId().equals(id)) {
-                model.addAttribute("user", user);
-            }
-        }
+    public String viewUserProfile(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.findById(id).orElseThrow();
+        model.addAttribute("user", user);
         return "user/profile";
     }
 
     @GetMapping("/{id}/form")
-    public String updateForm(@PathVariable("id")String id, Model model) {
-        for (User user : users) {
-            if (user.getUserId().equals(id)) {
-                model.addAttribute("user", user);
-            }
-        }
+    public String updateForm(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.findById(id).orElseThrow();
+        model.addAttribute("user", user);
         return "user/updateForm";
     }
 }
