f5aaaf0b7ee22930fe9d67c46e9056dad293f551
doopang24 <wodus4145@naver.com>
2025-03-21 20:33:29 +0900

    refactor: Article 수정
    생성자 추가, id 필드 추가

--- src/main/java/codesquad/codestagram/domain/Article.java
+++ src/main/java/codesquad/codestagram/domain/Article.java
@@ -1,10 +1,28 @@
 package codesquad.codestagram.domain;
 
+import java.time.LocalDateTime;
+
 public class Article {
 
+    private static long sequence = 0L;
+
+    private Long id;
     private String writer;
     private String title;
     private String contents;
+    private final LocalDateTime createAt;
+
+    public Article(String writer, String title, String contents) {
+        this.id = ++sequence;
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+        this.createAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
 
     public String getWriter() {
         return writer;
@@ -29,4 +47,9 @@ public class Article {
     public void setContents(String contents) {
         this.contents = contents;
     }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
 }
