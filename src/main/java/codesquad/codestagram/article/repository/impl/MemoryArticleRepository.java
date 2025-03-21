be0cca0a2f8542bb2d2ce62dda263db1719e5c73
jaehoonchoi <eeoos8840@gmail.com>
2025-03-21 10:15:05 +0900

    Feat(MemoryArticleRepository): ArticleRepository의 구현체 생성

--- src/main/java/codesquad/codestagram/article/repository/impl/MemoryArticleRepository.java
+++ src/main/java/codesquad/codestagram/article/repository/impl/MemoryArticleRepository.java
@@ -1,2 +1,37 @@
-package codesquad.codestagram.article.repository.impl;public class MemoryArticleRepository {
+package codesquad.codestagram.article.repository.impl;
+
+import codesquad.codestagram.article.domain.Article;
+import codesquad.codestagram.article.repository.ArticleRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MemoryArticleRepository implements ArticleRepository {
+
+    private static final Map<Long, Article> store = new ConcurrentHashMap<>();
+    private static final AtomicLong sequence = new AtomicLong(0L);
+
+    @Override
+    public void save(Article article) {
+        article.setId(sequence.incrementAndGet());
+        store.put(article.getId(), article);
+    }
+
+    @Override
+    public Optional<Article> findById(Long articleId) {
+        return Optional.ofNullable(store.get(articleId));
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
 }
