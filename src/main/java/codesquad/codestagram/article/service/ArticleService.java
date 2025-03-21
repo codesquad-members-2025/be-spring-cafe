5d4fa88b9062402c59aac0e5df65068bcaa2ab17
jaehoonchoi <eeoos8840@gmail.com>
2025-03-21 10:01:31 +0900

    Feat(ArticleService): ArticleService 생성
    - 질문 등록, 조회, 모든 질문 조회 메서드 생성

--- src/main/java/codesquad/codestagram/article/service/ArticleService.java
+++ src/main/java/codesquad/codestagram/article/service/ArticleService.java
@@ -1,2 +1,32 @@
-package codesquad.codestagram.article.service;public class ArticleService {
+package codesquad.codestagram.article.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Autowired
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public void register(Article article) {
+        articleRepository.save(article);
+    }
+
+    public List<Article> findArticles() {
+        return articleRepository.findAll();
+    }
+
+    public Article findArticle(Long articleId) {
+        return articleRepository.findById(articleId).orElseThrow(
+                () -> new NoSuchElementException("존재하지 않는 질문입니다.")
+        );
+    }
+
 }
