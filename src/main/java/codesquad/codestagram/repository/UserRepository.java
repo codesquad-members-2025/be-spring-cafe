30ec711ac707c3e38dd016c0c9b05b90c83e4fab
jang-jinuk <dlsndifn98@gmail.com>
2025-03-21 10:24:19 +0900

    feat: 회원 정보 수정 기능 로직 추가
    save 메소드에 해당 회원 id가 존재할 시 회원 정보를 수정하는 기능을 수행하도록 구현했습니다.

--- src/main/java/codesquad/codestagram/repository/UserRepository.java
+++ src/main/java/codesquad/codestagram/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package codesquad.codestagram.repository;
 
-import codesquad.codestagram.domain.Article;
 import codesquad.codestagram.domain.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class UserRepository {
@@ -15,6 +15,14 @@ public class UserRepository {
     private static Long sequence = 0L;
 
     public User save(User user) {
+        if (user.getId() != null) { // id 값이 존재하면 수정
+            User existingUser = findById(user.getId()).orElseThrow();
+            existingUser.setName(user.getName());
+            existingUser.setPassword(user.getPassword());
+            users.set(user.getId().intValue() - 1, existingUser);
+            return existingUser;
+        }
+
         user.setId(++sequence);
         users.add(user);
         return user;
