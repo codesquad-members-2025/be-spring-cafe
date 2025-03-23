916cb39833558cd75bea8fde92eddb442a9adbf4
JangSuBin <109069277+longrunBiin@users.noreply.github.com>
2025-03-24 08:58:58 +0900

    [빙빙] step5-1 로그인 구현 (#24)
    * docs : 4단계 설계
    - 로그인 로직 설계
    
    * feat : 로그인 페이지 작성
    
    * feat : 로그인 기능 구현
    - UserRepository::findByUserIdAndPassword 추가
    - 아이디와 비밀번호가 일치하지 않으면 에러메세지 출력
    - 세션에 유저 데이터 등록
    
    * feat : 로그아웃 기능 구현
    - 로그아웃 버튼 클릭시 세션 삭제
    - 쿠키 JSESSIONID삭제
    - 로그인시 버튼 목록변경
    
    * refactor : 컨트롤러 로직 서비스로 이동
    - 리터럴 상수로 변경
    
    * feat : checkEqualTo 구현
    - 비밀번호 일치여부를 반환하는 메소드 구현
    
    * refactor : 의미없는 변수 제거 및 이름 변경
    
    * refactor : 테스트 데이터 생성
    - 회원가입시 중복아이디가 있는경우 에러를 반환하도록 수정
    
    * feat : 테스트용 데이터 추가
    
    * fix : 회원가입시 분기 처리 수정
    - 중복회원이 없는 경우 에러를 날리던 방식을 isPresent를 사용하여 수정
    
    * test : 회원가입시 중복 아이디 테스트
    - 아이디가 중복되는 경우 예외 발생을 테스트한다.
    - mockito 라이브러리 의존성 추가
    
    * test : UserServiceTest 작성
    - 회원가입 성공테스트
    - 유저 리스트 조회 테스트
    - 유저 정보 수정 테스트
    
    * test : update시 save 호출 테스트 추가
    
    * refactor : 오류메세지 상수 서비스로 이동
    - 오류메세지 상수는 서비스에서 사용하고 컨트롤러에서는 에러 메세지를 받도록 수정
    
    * chore : jpa.hibernate.ddl-auto update로 변경
    
    * docs : readme.md 내용추가
    - 트러블슈팅 추가
    - 테스트 내용 추가
    
    * fix : MvcConfig /login 템플릿 경로 수정
    - 경로 앞에 "/" 제거

--- src/main/java/codesquad/codestagram/controller/UserController.java
+++ src/main/java/codesquad/codestagram/controller/UserController.java
@@ -2,10 +2,11 @@ package codesquad.codestagram.controller;
 
 import codesquad.codestagram.domain.User;
 import codesquad.codestagram.dto.UserDto;
-import codesquad.codestagram.repository.UserRepository;
-import java.util.ArrayList;
+import codesquad.codestagram.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,63 +18,62 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
-    public static final String WRONG_PASSWORD = "비밀번호가 틀렸습니다.";
-    private final UserRepository userRepository;
+    public static final String USER = "user";
+    public static final String SESSIONED_USER = "sessionedUser";
+    public static final String PASSWORD_VALID = "passwordValid";
+    public static final String ERROR_MESSAGE = "errorMessage";
+    public static final String JSESSION_ID = "JSESSIONID";
+    public static final String ROOT_DIRECTORY = "/";
+    public static final int ONE_HOUR = 3600;
+    public static final int ZERO  = 0;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/users")
     public String signUp(@ModelAttribute UserDto.UserRequestDto requestDto, Model model) {
-        // user_id 중복 검사
-        Optional<User> existingUser = userRepository.findByUserId(requestDto.getUserId());
-
-        if (existingUser.isPresent()) {
+        try{
+            userService.joinUser(requestDto);
+        }catch (IllegalArgumentException e){
             // 이미 존재하는 user_id일 경우
-            model.addAttribute("errorMessage", "이미 존재하는 사용자 ID입니다.");
+            model.addAttribute(ERROR_MESSAGE, e.getMessage());
             return "register";  // 회원가입 페이지로 다시 돌아감
         }
-
-        // 중복되지 않으면 사용자 저장
-        User user = requestDto.toUser();
-        userRepository.save(user);
-
         return "redirect:/users";
     }
 
     @GetMapping("/users")
     public String showUsers(Model model) {
-        List<User> users = userRepository.findAll();
-        model.addAttribute("users", users);
+        List<User> users = userService.getUserList();
+        model.addAttribute(USER, users);
         return "user/list";
     }
 
     @GetMapping("/users/{id}")
     public String showUserProfile(@PathVariable Long id, Model model) {
-        User user = userRepository.findById(id).get();
-        model.addAttribute("user", user);
-        model.addAttribute("id", id);
+        User user = userService.getUserById(id);
+        model.addAttribute(USER, user);
         return "user/profile";
     }
 
     @GetMapping("/users/{id}/update")
     public String editUser(Model model, @PathVariable Long id) {
-        User user = userRepository.findById(id).get();
-        model.addAttribute("user", user);
+        User user = userService.getUserById(id);
+        model.addAttribute(USER, user);
         return "user/updateForm";  // 정보 수정 페이지로 이동
     }
 
     @GetMapping("/users/verify_password")
     public String verifyPassword(@RequestParam String password, @RequestParam Long id, Model model) {
-        User user = userRepository.findById(id).get();
-        model.addAttribute("user", user);
+        User user = userService.getUserById(id);
+        model.addAttribute(USER, user);
 
-        if (user.getPassword().equals(password)) {
-            model.addAttribute("passwordValid", true);
+        if (user.checkEqualTo(password)) {
+            model.addAttribute(PASSWORD_VALID, true);
         }else {
-            model.addAttribute("passwordValid", false);
-            model.addAttribute("error", WRONG_PASSWORD);
+            model.addAttribute(PASSWORD_VALID, false);
         }
 
         return "user/updateForm"; // 실패 시 프로필 화면으로 돌아가기
@@ -81,12 +81,40 @@ public class UserController {
 
     @PutMapping("users/update")
     public String updateProfile(@RequestParam String name, @RequestParam String email, @RequestParam Long id, Model model) {
-        User user = userRepository.findById(id).get();
-        user.updateUser(name, email);
-        userRepository.save(user);
+        User user = userService.updateUser(id, name, email);
 
-        model.addAttribute("user", user);
+        model.addAttribute(USER, user);
         return "redirect:/users";
     }
 
+    @PostMapping("users/sign-in")
+    public String signInUser(@RequestParam String userId, @RequestParam String password,
+                             HttpSession httpSession, Model model) {
+        try {
+            //아이디와 비밀번호가 일치하는 User가 없으면 에러 출력
+            User user = userService.getUserForLogin(userId, password);
+            //세션에 user데이터 등록
+            httpSession.setAttribute(SESSIONED_USER, user);
+            // 세션 설정
+            httpSession.setMaxInactiveInterval(ONE_HOUR); // 1시간
+        }catch (IllegalArgumentException e){
+            model.addAttribute(ERROR_MESSAGE, e.getMessage());
+            return "user/signIn";
+        }
+
+        return "redirect:/";  // 로그인 성공시 홈페이지로 리다이렉트
+    }
+
+    @GetMapping("users/sign-out")
+    public String logout(HttpSession httpSession, HttpServletResponse response){
+        httpSession.invalidate();
+
+        // 클라이언트 쿠키에서 세션 ID 삭제
+        Cookie cookie = new Cookie(JSESSION_ID, null); // 세션 쿠키 삭제
+        cookie.setMaxAge(ZERO);  // 즉시 만료
+        cookie.setPath(ROOT_DIRECTORY);   // 모든 경로에서 적용
+        response.addCookie(cookie);
+
+        return "redirect:/";
+    }
 }
---
+++ src/main/java/codesquad/codestagram/service/UserService.java
@@ -0,0 +1,55 @@
+package codesquad.codestagram.service;
+
+import codesquad.codestagram.domain.User;
+import codesquad.codestagram.dto.UserDto.UserRequestDto;
+import codesquad.codestagram.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public static final String USER_ALREADY_EXIST = "이미 존재하는 사용자 ID입니다.";
+    public static final String FAIL_SING_IN = "아이디와 비밀번호가 일치하지 않습니다.";
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostConstruct
+    public void initDB() {
+        userRepository.save(new User("javajigi", "test", "자바지기", "javajigi@slipp.net"));
+        userRepository.save(new User("sanjigi", "test", "산지기", "sanjigi@slipp.net"));
+    }
+
+    public void joinUser(UserRequestDto requestDto) {
+        //같은 아이디를 가지는 유저가 있는지 확인
+        if(userRepository.findByUserId(requestDto.getUserId()).isPresent())
+                throw new IllegalArgumentException(USER_ALREADY_EXIST);
+
+        // 중복되지 않으면 사용자 저장
+        User user = requestDto.toUser();
+        userRepository.save(user);
+    }
+
+    public List<User> getUserList() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    public User updateUser(Long id, String name, String email) {
+        User user = userRepository.findById(id).get();
+        user.updateUser(name, email);
+        userRepository.save(user);
+
+        return user;
+    }
+
+    public User getUserForLogin(String userId, String password) {
+       return userRepository.findByUserIdAndPassword(userId, password).orElseThrow(() -> new IllegalArgumentException(FAIL_SING_IN));
+    }
+}
