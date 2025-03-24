609d455f3adc04b86d0c1c1324d34148b035305f
2jiyong <jdragon2000@naver.com>
2025-03-24 11:58:41 +0900

    Refactor: setter 제거 및 기본 생성자 접근제어자 변경

--- src/main/java/codesquad/codestagram/domain/Article.java
+++ src/main/java/codesquad/codestagram/domain/Article.java
@@ -33,32 +33,16 @@ public class Article {
         return user;
     }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getContent() {
         return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getCreatedAt() {
