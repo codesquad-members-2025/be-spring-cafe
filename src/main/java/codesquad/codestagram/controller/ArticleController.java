74e59d8f029cb1bf4ad3ca10324289cb230ba51c
jang-jinuk <dlsndifn98@gmail.com>
2025-03-24 19:56:12 +0900

    refactor: ArticleController와 MainController 통합
    - MainController에 GET / 요청 처리를 해주는 getMain 메소드를 ArticleController에 이동시켰습니다.

--- src/main/java/codesquad/codestagram/controller/ArticleController.java
+++ src/main/java/codesquad/codestagram/controller/ArticleController.java
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/article")
 public class ArticleController {
 
     private final ArticleRepository articleRepository;
@@ -17,13 +18,20 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
-    @PostMapping
+    @GetMapping("/")
+    public String viewMain(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "main";
+    }
+
+    @PostMapping("/article")
     public String writeArticle(@ModelAttribute Article article) {
         articleRepository.save(article);
         return "redirect:/";
     }
 
-    @GetMapping("/{index}")
+    @GetMapping("/article/{index}")
     public String viewArticle(@PathVariable("index") Long index, Model model) {
         Article article = articleRepository.findById(index).orElseThrow();
         model.addAttribute("article", article);
