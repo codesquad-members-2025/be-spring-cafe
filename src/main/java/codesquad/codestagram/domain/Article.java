6be5ece5ee98b9fbd84782bdcaa2fc30b872aae1
jang-jinuk <dlsndifn98@gmail.com>
2025-03-24 13:56:19 +0900

    feat: ArticleRepository JPA 상속
    - ArticleRepository가 JpaRepository를 상속하도록 했습니다.
    - 기존 메소드는 JpaReopository에 존재하기 때문에 제거했습니다.
    - Article 클래스를 Entity로 등록했습니다.

--- src/main/java/codesquad/codestagram/domain/Article.java
+++ src/main/java/codesquad/codestagram/domain/Article.java
@@ -1,8 +1,19 @@
 package codesquad.codestagram.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String content;
 
     public Long getId() {
--- src/main/java/codesquad/codestagram/repository/ArticleRepository.java
+++ src/main/java/codesquad/codestagram/repository/ArticleRepository.java
@@ -1,32 +1,9 @@
 package codesquad.codestagram.repository;
 
 import codesquad.codestagram.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public class ArticleRepository {
-
-    private static List<Article> articles = Collections.synchronizedList(new ArrayList<>());
-    private static Long sequence = 0L;
-
-    public Article save(Article article) {
-        article.setId(++sequence);
-        articles.add(article);
-        return article;
-    }
-
-    public Optional<Article> findById(Long id) {
-        return articles.stream()
-            .filter(article -> article.getId().equals(id))
-            .findAny();
-    }
-
-    public List<Article> findAll() {
-        return articles;
-    }
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
