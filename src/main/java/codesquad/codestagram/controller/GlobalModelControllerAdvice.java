955528c163f0ce5f30d4908f648e006ffd6d76f3
2jiyong <jdragon2000@naver.com>
2025-03-25 16:31:29 +0900

    Feat: 전역 컨트롤러가 에러를 잡도록 수정

--- src/main/java/codesquad/codestagram/controller/GlobalModelControllerAdvice.java
+++ src/main/java/codesquad/codestagram/controller/GlobalModelControllerAdvice.java
@@ -1,10 +1,17 @@
 package codesquad.codestagram.controller;
 
 import codesquad.codestagram.domain.User;
+import codesquad.codestagram.exception.ArticleNotFoundException;
+import codesquad.codestagram.exception.DuplicateUserIdException;
+import codesquad.codestagram.exception.InvalidPasswordException;
+import codesquad.codestagram.exception.UserNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalModelControllerAdvice {
@@ -15,5 +22,29 @@ public class GlobalModelControllerAdvice {
         if(user != null) {
             model.addAttribute("loginUser", user);
         }
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    public String handleInvalidPassword(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMessage", "로그인에 실패했습니다.");
+        return "redirect:" + request.getHeader("Referer");
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public String handleUserNotFound(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMessage", "유저를 찾을 수 없습니다.");
+        return "redirect:" + request.getHeader("Referer");
+    }
+
+    @ExceptionHandler(ArticleNotFoundException.class)
+    public String handleArticleNotFound(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMessage", "게시물을 찾을 수 없습니다.");
+        return "redirect:" + request.getHeader("Referer");
+    }
+
+    @ExceptionHandler(DuplicateUserIdException.class)
+    public String handleDuplicateUserId(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMessage", "이미 동일한 아이디의 사용자가 있습니다.");
+        return "redirect:" + request.getHeader("Referer");
     }
 }
