d19b4c6a6c04e9c0b0e92ec504563eccd2768abd
2jiyong <jdragon2000@naver.com>
2025-03-25 16:49:55 +0900

    Feat: 상세한 에러메시지 추가

--- src/main/java/codesquad/codestagram/service/UserService.java
+++ src/main/java/codesquad/codestagram/service/UserService.java
@@ -10,7 +10,6 @@ import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -21,28 +20,28 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User join(UserForm userform){
+    public User join(UserForm userform) {
         Optional<User> result = userRepository.findByUserId(userform.getUserId());
-        result.ifPresent(u-> {
-            throw new DuplicateUserIdException(u.getUserId());
+        result.ifPresent(u -> {
+            throw new DuplicateUserIdException("ID가 " + u.getUserId() + "인 사용자가 이미 존재합니다.");
         });
         return userRepository.save(userform.toEntity());
     }
 
-    public List<User> findAllUsers(){
+    public List<User> findAllUsers() {
         return userRepository.findAll();
     }
 
-    public User findByUserId(String userId){
+    public User findByUserId(String userId) {
         return userRepository.findByUserId(userId)
-                .orElseThrow(() -> new UserNotFoundException(userId));
+                .orElseThrow(() -> new UserNotFoundException("ID가 " + userId + "인 사용자가 존재하지 않습니다."));
     }
 
     @Transactional
-    public boolean updateUser(User loginUser, UserForm userForm){
+    public boolean updateUser(User loginUser, UserForm userForm) {
         String userId = userForm.getUserId();
         User user = findByUserId(userId);
-        if(!user.equals(loginUser)) {
+        if (!user.equals(loginUser)) {
             return false;
         }
         return user.updateIfPasswordValid(
@@ -52,9 +51,9 @@ public class UserService {
                 userForm.getEmail());
     }
 
-    public User userLogin(String userId, String password){
+    public User userLogin(String userId, String password) {
         User user = findByUserId(userId);
-        if(user.isPasswordValid(password)){
+        if (user.isPasswordValid(password)) {
             return user;
         }
         throw new InvalidPasswordException();
