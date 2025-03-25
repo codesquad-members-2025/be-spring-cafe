6f2164b3a7bb017b83978dde60f49172836c3844
subin <eltm521@naver.com>
2025-03-25 10:49:49 +0900

    fix : AuthController @Controller없던 문제 수정

--- src/main/java/codesquad/codestagram/controller/AuthController.java
+++ src/main/java/codesquad/codestagram/controller/AuthController.java
@@ -8,12 +8,14 @@ import codesquad.codestagram.service.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class AuthController {
     public static final String SESSIONED_USER = "sessionedUser";
     public static final String JSESSION_ID = "JSESSIONID";
