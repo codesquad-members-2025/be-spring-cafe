750fdce2afd2dd8d8a410d5ba098f31de7980275
jang-jinuk <dlsndifn98@gmail.com>
2025-03-26 20:50:13 +0900

    feat: existUserByLoginId 메소드 추가
    중복되는 로그인 아이디가 존재하는지 확인하는 메소드를 추가했습니다.

--- src/main/java/codesquad/codestagram/repository/UserRepository.java
+++ src/main/java/codesquad/codestagram/repository/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByLoginId(String loginId);
+    boolean existsUserByLoginId(String loginId);
 }
