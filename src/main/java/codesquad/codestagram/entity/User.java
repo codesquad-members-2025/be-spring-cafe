71d1787d3fe5c021ea2e236ce38bdcc3ea63ceec
dorkem <20233144@gs.cwnu.ac.kr>
2025-03-26 14:33:05 +0900

    :sparkles: user객체 재생성
    seq라는 변수가 헷갈려서 변경하였습니다.

---
+++ src/main/java/codesquad/codestagram/entity/User.java
@@ -0,0 +1,72 @@
+package codesquad.codestagram.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userid;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    public User() {}
+
+    public User(String userid, String password, String email, String name) {
+        this.userid = userid;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
