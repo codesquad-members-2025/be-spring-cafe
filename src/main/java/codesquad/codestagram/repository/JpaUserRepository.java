4f5b8e36aa9cc29f93c09323a7db23ea48c1c35c
doopang24 <wodus4145@naver.com>
2025-03-26 11:40:38 +0900

    feat: JpaUserRepository 추가
    JPA를 사용할 때마다 데이터베이스와 통신하는 클래스 구현

---
+++ src/main/java/codesquad/codestagram/repository/JpaUserRepository.java
@@ -0,0 +1,42 @@
+package codesquad.codestagram.repository;
+
+import codesquad.codestagram.domain.User;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaUserRepository implements UserRepository {
+
+    public final EntityManager em;
+
+    public JpaUserRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public User save(User user) {
+        em.persist(user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        User user = em.find(User.class, id);
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public Optional<User> findByUserId(String userId) {
+        List<User> result = em.createQuery("select u from User u where u.userId = :userId", User.class)
+                .setParameter("userId", userId)
+                .getResultList();
+
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return em.createQuery("select m from User u", User.class).getResultList();
+    }
+}
