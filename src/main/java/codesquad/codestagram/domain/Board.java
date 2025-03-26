f010f591e4c8dd1cdb8191703967ebb7e0d63b14
Gyuwon Moon <gyuwon822@gmail.com>
2025-03-26 11:10:22 +0900

    feat: Board에 엔티티 매핑

--- src/main/java/codesquad/codestagram/domain/Board.java
+++ src/main/java/codesquad/codestagram/domain/Board.java
@@ -1,12 +1,16 @@
 package codesquad.codestagram.domain;
 
+import jakarta.persistence.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
 public class Board {
     private Long boardId;
     private String title;
     private String content;
     private String writer;
     //아직 로그인 기능 없음 -> writer만 저장
-
+    @Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getBoardId() {
         return boardId;
     }
