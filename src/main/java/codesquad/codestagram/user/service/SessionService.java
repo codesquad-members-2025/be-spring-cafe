80d9c2b421edfbe6b353d8e79d215c27cff655b7
jaehoonchoi <eeoos8840@gmail.com>
2025-03-27 21:33:48 +0900

    Feat(SessionService): SessionService 추가
    - 세션에 User 객체를 저장하는 로그인
    - 세션에서 User 객체를 삭제하는 로그아웃
    - 세션에서 유저를 조회하는 getLoggedInUser
    - 로그인 여부 확인 메서드인 isLoggedIn

---
+++ src/main/java/codesquad/codestagram/user/service/SessionService.java
@@ -0,0 +1,27 @@
+package codesquad.codestagram.user.service;
+
+import codesquad.codestagram.user.domain.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessionService {
+
+    private static final String USER_SESSION_KEY = "LOGGED_IN_USER";
+
+    public void login(HttpSession session, User user) {
+        session.setAttribute(USER_SESSION_KEY, user);
+    }
+
+    public void logout(HttpSession session) {
+        session.removeAttribute(USER_SESSION_KEY);
+    }
+
+    public User getLoggedInUser(HttpSession session) {
+        return (User) session.getAttribute(USER_SESSION_KEY);
+    }
+
+    public boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute(USER_SESSION_KEY) != null;
+    }
+}
