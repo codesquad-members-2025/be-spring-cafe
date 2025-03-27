0a87786873af4d327ba70e081271f2939173d48b
jang-jinuk <dlsndifn98@gmail.com>
2025-03-27 21:55:00 +0900

    refactor: UserController 비즈니스 로직 분리
    - UserController에 비즈니스 로직이 복잡해져서 비즈니스 로직을 UserService에 분리했습니다.

--- src/main/java/codesquad/codestagram/controller/UserController.java
+++ src/main/java/codesquad/codestagram/controller/UserController.java
@@ -1,9 +1,7 @@
 package codesquad.codestagram.controller;
 
 import codesquad.codestagram.domain.User;
-import codesquad.codestagram.repository.UserRepository;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
+import codesquad.codestagram.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -18,98 +16,108 @@ import java.util.Objects;
 @RequestMapping("/users")
 public class UserController {
 
-    private final UserRepository userRepository;
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping
     public String signup(@ModelAttribute User user, RedirectAttributes redirectAttributes) {
-        if (userRepository.existsUserByLoginId(user.getLoginId())) {
-           redirectAttributes.addFlashAttribute("message","이미 존재하는 아이디입니다.");
-           return "redirect:/users/form";
+
+        try {
+            userService.createUser(user);
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+            return "redirect:/users/form";
         }
-        userRepository.save(user);
+
         return "redirect:/users";
     }
 
     @GetMapping
     public String viewUserList(Model model) {
-        List<User> users = userRepository.findAll();
+        List<User> users = userService.getUserList();
         model.addAttribute("users", users);
         return "user/list";
     }
 
     @GetMapping("/{id}")
-    public String viewUserProfile(@PathVariable("id") Long id, Model model) {
-        User user = userRepository.findById(id).orElseThrow();
-        model.addAttribute("user", user);
+    public String viewUserProfile(@PathVariable("id") Long id, Model model, RedirectAttributes redirectAttributes) {
+
+        try {
+            User user = userService.getUser(id);
+            model.addAttribute("user", user);
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+            return "redirect:/";
+        }
         return "user/profile";
     }
 
     @GetMapping("/{id}/form")
     public String viewUpdateForm(@PathVariable("id") Long id, Model model, HttpSession session
         , RedirectAttributes redirectAttributes) {
+
         if (session.getAttribute("loginUser") == null) { //로그인 상태인지 확인
             return "redirect:/users/loginForm";
         }
 
         User loginUser = (User) session.getAttribute("loginUser");
-        User findUser = userRepository.findById(id).orElseThrow();
 
-        if (!Objects.equals(loginUser.getId(), findUser.getId())) {
-            redirectAttributes.addFlashAttribute("alertMessage", "잘못된 접근입니다.");
+        try {
+            User findUser = userService.getUser(id);
+
+            if (loginUser.getId() != findUser.getId()) { //URL을 직접 입력해서 타인의 회원정보를 수정할 경우
+                throw new IllegalArgumentException("잘못된 접근입니다.");
+            }
+            model.addAttribute("user", findUser);
+
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("alertMessage", e.getMessage());
             return "redirect:/";
         }
 
-        model.addAttribute("user", findUser);
         return "user/updateForm";
     }
 
     @PutMapping("/{id}/update")
     public String updateUser(@PathVariable("id") Long id, @ModelAttribute User user, @RequestParam String confirmPassword
         , RedirectAttributes redirectAttributes) {
-        User existingUser = userRepository.findById(id).orElseThrow();
 
-        if (existingUser.getPassword().equals(confirmPassword)) {
-            userRepository.save(user);
-            return "redirect:/users";
+        boolean updateResult = userService.updateUser(user, id, confirmPassword);
+
+        if (!updateResult) {
+            redirectAttributes.addFlashAttribute("message", "비밀번호가 일치하지 않습니다.");
+            return "redirect:/users/" + id + "/form";
         }
-        redirectAttributes.addFlashAttribute("message", "비밀번호가 일치하지 않습니다.");
-        return "redirect:/users/" + id + "/form";
+
+        return "redirect:/users/" + id;
     }
 
     @PostMapping("/login")
     public String login(@RequestParam String loginId, @RequestParam String password, HttpSession session
         , RedirectAttributes redirectAttributes) {
+        try {
+            boolean loginResult = userService.login(loginId, password, session);
 
-        if (!userRepository.existsUserByLoginId(loginId)) {
-            redirectAttributes.addFlashAttribute("message", "아이디가 존재하지 않습니다.");
+            if (!loginResult) {
+                redirectAttributes.addFlashAttribute("message", "비밀번호가 틀렸습니다.");
+                return "redirect:/users/loginForm";
+            }
+
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
             return "redirect:/users/loginForm";
         }
-
-        User findUser = userRepository.findByLoginId(loginId);
-
-        if (!password.equals(findUser.getPassword())) {
-            redirectAttributes.addFlashAttribute("message", "비밀번호가 일치하지 않습니다.");
-            return "redirect:/users/loginForm";
-        }
-
-        session.setAttribute("loginUser", findUser);
         return "redirect:/";
     }
 
     @PostMapping("/logout")
     public String logout(HttpSession session, HttpServletResponse response) {
-
         if (session != null) {
             session.invalidate();
         }
-
-        Cookie cookie = new Cookie("JSESSIONID", null);
-        cookie.setMaxAge(0);
-        cookie.setPath("/");
-        response.addCookie(cookie);
 
         return "redirect:/";
     }
