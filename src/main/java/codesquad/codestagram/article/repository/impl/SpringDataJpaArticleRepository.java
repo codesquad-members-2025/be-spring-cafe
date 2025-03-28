a135b4136654468249f95a751d015d9495793759
jaehoonchoi <eeoos8840@gmail.com>
2025-03-28 14:25:12 +0900

    Remove: ArticleRepository 구현체 삭제

--- src/main/java/codesquad/codestagram/article/repository/impl/SpringDataJpaArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package codesquad.codestagram.article.repository.impl;
-
-import codesquad.codestagram.article.domain.Article;
-import codesquad.codestagram.article.repository.ArticleRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpringDataJpaArticleRepository extends JpaRepository<Article, Long>, ArticleRepository {
-}
