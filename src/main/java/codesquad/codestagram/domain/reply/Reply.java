347bbd7c3b318720a31cb892a8d8543f3aab4833
gladhee <gladhee@icloud.com>
2025-03-28 11:44:33 +0900

    feat(reply): Reply Entity 작성

---
+++ src/main/java/codesquad/codestagram/domain/reply/Reply.java
@@ -0,0 +1,55 @@
+package codesquad.codestagram.domain.reply;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reply")
+public class Reply {
+
+    @Id
+    private Long id;
+
+    private Long userId;
+    private Long articleId;
+    private String content;
+
+    public Reply(Long userId, Long articleId, String content) {
+        this.userId = userId;
+        this.articleId = articleId;
+        this.content = content;
+    }
+
+    public Reply() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getArticleId() {
+        return articleId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isSameWriter(Long userId) {
+        return this.userId.equals(userId);
+    }
+
+    public boolean isSameArticle(Long articleId) {
+        return this.articleId.equals(articleId);
+    }
+
+}
