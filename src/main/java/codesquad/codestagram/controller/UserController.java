31316dc9c38ad7ab5c33ee9b6c7c99283e515916
doopang24 <wodus4145@naver.com>
2025-03-28 14:58:57 +0900

    feat: UserController 수정
    로그인 기능 구현

--- src/main/java/codesquad/codestagram/controller/UserController.java
+++ src/main/java/codesquad/codestagram/controller/UserController.java
@@ -2,12 +2,14 @@ package codesquad.codestagram.controller;
 
 import codesquad.codestagram.domain.User;
 import codesquad.codestagram.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -67,5 +69,18 @@ public class UserController {
         userService.join(user);
         return "redirect:/users";
     }
+
+    @PostMapping("/user/login")
+    public String login(@RequestParam("userId") String userId,
+                        @RequestParam("password") String password,
+                        HttpSession session) {
+        Optional<User> user = userService.findOneUser(userId);
+        if(user.isPresent() && user.get().getPassword().equals(password)) {
+            session.setAttribute("loginUser", user.get());  // 세션에 로그인 사용자 저장, setAttribute 는 세션에 데이터 저장할 때 사용
+            return "redirect:/";
+        }
+        return "user/login_failed";  // 로그인 실패 시 어떤 걸 리턴할 것인가?
+    }
+
 
 }
