73541f945d2e9c859e12aae5474c860048bdcc65
2jiyong <jdragon2000@naver.com>
2025-03-28 14:55:41 +0900

    Feat: id로 reply 찾는 메서드 추가

--- src/main/java/codesquad/codestagram/repository/JpaReplyRepository.java
+++ src/main/java/codesquad/codestagram/repository/JpaReplyRepository.java
@@ -5,6 +5,7 @@ import codesquad.codestagram.domain.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JpaReplyRepository extends ReplyRepository, JpaRepository<Reply, Long> {
 
@@ -16,4 +17,7 @@ public interface JpaReplyRepository extends ReplyRepository, JpaRepository<Reply
 
     @Override
     void delete(Reply reply);
+
+    @Override
+    Optional<Reply> findById(Long id);
 }
--- src/main/java/codesquad/codestagram/repository/ReplyRepository.java
+++ src/main/java/codesquad/codestagram/repository/ReplyRepository.java
@@ -4,9 +4,11 @@ import codesquad.codestagram.domain.Article;
 import codesquad.codestagram.domain.Reply;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReplyRepository {
     Reply save(Reply reply);
     List<Reply> findByArticle(Article article);
+    Optional<Reply> findById(Long id);
     void delete(Reply reply);
 }
