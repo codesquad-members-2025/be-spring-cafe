d4ce1d97836a943e49badabeb33c5e62288d8db8
2jiyong <jdragon2000@naver.com>
2025-03-28 11:29:55 +0900

    Feat: Reply 엔티티 생성

---
+++ src/main/java/codesquad/codestagram/domain/Reply.java
@@ -0,0 +1,58 @@
+package codesquad.codestagram.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+public class Reply {
+
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    private String text;
+    private LocalDateTime createdAt;
+
+    protected Reply() {}
+
+    public Reply(Article article, User user, String text) {
+        this.article = article;
+        this.user = user;
+        this.text = text;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getCreatedAt() {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return createdAt.format(dateFormatter)+" "+createdAt.format(timeFormatter);
+    }
+}
+
