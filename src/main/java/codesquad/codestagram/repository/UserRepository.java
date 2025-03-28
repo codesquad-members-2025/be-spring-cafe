f7298c6ee1bb94a24f806f925e7a3b970c6122a1
jang-jinuk <dlsndifn98@gmail.com>
2025-03-28 10:47:24 +0900

    Refactor: findByLoginId() 반환값을 Optional 타입으로 변경
    - null 반환 가능성을 대비하여 Optional 타입으로 반환하여 null 안정성을 높였습니다.

--- src/main/java/codesquad/codestagram/repository/UserRepository.java
+++ src/main/java/codesquad/codestagram/repository/UserRepository.java
@@ -4,8 +4,10 @@ import codesquad.codestagram.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByLoginId(String loginId);
+    Optional<User> findByLoginId(String loginId);
     boolean existsUserByLoginId(String loginId);
 }
