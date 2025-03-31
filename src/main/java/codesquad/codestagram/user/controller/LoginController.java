02824d6070265b6daa5cf55c124052e6474ae221
jaehoonchoi <eeoos8840@gmail.com>
2025-03-31 21:58:48 +0900

    Refactor: 로그아웃 요청 변경
    - GET 요청에서 POST 요청으로 변경
    - 그에 따른 html 폼 수정

--- src/main/java/codesquad/codestagram/user/controller/LoginController.java
+++ src/main/java/codesquad/codestagram/user/controller/LoginController.java
@@ -51,7 +51,7 @@ public class LoginController {
 
     //@PostMapping이 맞나?
     //회원가입 후 바로 로그인 되게끔하자
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public String logout(HttpSession session) {
         sessionService.logout(session);
         return "redirect:/";
