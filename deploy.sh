cf624bdf7ce031a51a8c9015077a5f4a7f6bc217
2jiyong <jdragon2000@naver.com>
2025-04-01 11:34:33 +0900

    Fix: deploy.sh 실행권한 변경하고, db 마운팅 추가

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 IMAGE_NAME=my-app
 CONTAINER_NAME=my-app-container
+H2_DATA_DIR=/home/ec2-user/h2-data
 
 echo "🚀 Docker 이미지 빌드 중..."
 docker build -t $IMAGE_NAME .
@@ -14,6 +15,7 @@ echo "🚢 새 컨테이너 실행 중..."
 docker run -d \
   --name $CONTAINER_NAME \
   -p 80:8080 \
+  -v $H2_DATA_DIR:/h2-data \
   $IMAGE_NAME
 
 echo "✅ 배포 완료"
