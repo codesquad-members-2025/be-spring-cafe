3fc7de9252000338c2a7c7e4be9d2a40c479db84
jaehoonchoi <eeoos8840@gmail.com>
2025-04-01 12:35:55 +0900

    Feat(index.html): 게시글의 작성자 이름을 누르면 프로필 화면으로 가도록 기능 추가

--- src/main/java/codesquad/codestagram/user/domain/User.java
+++ src/main/java/codesquad/codestagram/user/domain/User.java
@@ -15,8 +15,6 @@ public class User {
     private String name;
     private String email;
 
-
-
     public User(String userId, String password, String name, String email) {
         this.userId = userId;
         this.password = password;
