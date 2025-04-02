044a068ef6f8d52b79a2e45afe9b7c655f1d41f9
dorkem <20233144@gs.cwnu.ac.kr>
2025-04-02 11:29:28 +0900

    :sparkles: my sql 의존성 추가
    의존성 추가 및 추가 및 sql쿼리 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, password, email, userid) VALUES ('a', 'a', 'a@a', 'bazzi');
+INSERT INTO users (name, password, email, userid) VALUES ('b', 'b', 'b@b', 'bazzi1');
+INSERT INTO users (name, password, email, userid) VALUES ('배찌', '배찌', 'w@w', '배찌');
+INSERT INTO posts (title, content, user_id) VALUES ('안녕', '내', 1);
