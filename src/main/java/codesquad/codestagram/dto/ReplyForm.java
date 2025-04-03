6b63be66b445118642971513e15f53bfa15468a9
Gyuwon Moon <gyuwon822@gmail.com>
2025-04-03 19:39:21 +0900

    feat: replyform dto 생성

---
+++ src/main/java/codesquad/codestagram/dto/ReplyForm.java
@@ -0,0 +1,12 @@
+package codesquad.codestagram.dto;
+
+public class ReplyForm {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
