0184aff2e892e44cd1f5b79dae0ce98096cfc0c0
gladhee <gladhee@icloud.com>
2025-04-04 12:57:13 +0900

    feat(ViewController): 페이지네이션 추가, ViewController 수정

--- src/main/java/codesquad/codestagram/domain/ViewController.java
+++ src/main/java/codesquad/codestagram/domain/ViewController.java
@@ -1,26 +1,38 @@
 package codesquad.codestagram.domain;
 
+import codesquad.codestagram.common.dto.PaginationDto;
 import codesquad.codestagram.domain.article.Article;
 import codesquad.codestagram.domain.article.ArticleRepository;
+import codesquad.codestagram.domain.article.ArticleService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class ViewController {
 
-    private final ArticleRepository articleRepository;
+    private final ArticleService articleService;
 
-    public ViewController(ArticleRepository articleRepository) {
-        this.articleRepository = articleRepository;
+    public ViewController(ArticleService articleService) {
+        this.articleService = articleService;
     }
 
     @GetMapping("")
-    public String index(Model model) {
-        List<Article> articles = articleRepository.findAllByDeleted(false);
+    public String index(@RequestParam(defaultValue = "0") int page,
+                        Model model) {
+        Page<Article> articles = articleService.findArticles(page);
+        PaginationDto pagination = PaginationDto.of(articles.getNumber() + 1,
+                articles.getSize(),
+                articles.getTotalElements());
+
         model.addAttribute("articles", articles);
+        model.addAttribute("pagination", pagination);
 
         return "index";
     }
